61fee91725dc1d2abd1128d73a49d4e18a0156fa
bienmek <ugolebgdu83@gmail.com>
2022-11-23 14:10:30 +0100

    genesis

---
+++ App.js
@@ -0,0 +1,14 @@
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import Landing from "./screens/Landing";
+
+const Stack = createStackNavigator()
+export default function App() {
+  return (
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName={"Landing"}>
+            <Stack.Screen name={"Landing"} component={Landing} options={{headerShown: false}}/>
+          </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
---
+++ screens/Landing.js
@@ -0,0 +1,10 @@
+import {SafeAreaView, Text, View} from "react-native";
+
+
+export default function Landing() {
+    return (
+        <SafeAreaView>
+            <Text>Hi its landing</Text>
+        </SafeAreaView>
+    )
+}
