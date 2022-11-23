239fd70329f2cdb702c2436c26d209e236d7a248
bienmek <ugolebgdu83@gmail.com>
2022-11-23 16:09:48 +0100

    Register done

--- screens/Register.js
+++ screens/Register.js
@@ -1,27 +1,77 @@
 import {SafeAreaView, Text, TextInput, TouchableOpacity, View, StyleSheet} from "react-native";
+import {useFonts} from "expo-font";
+import {useEffect, useState} from "react";
 
 
 export default function Register({navigation}) {
+    let [fontsLoaded] = useFonts({
+        oxygen_regular: require('../assets/font/Oxygen-Regular.ttf'),
+        oxygen_bold: require('../assets/font/Oxygen-Bold.ttf'),
+        oxygen_light: require('../assets/font/Oxygen-Light.ttf'),
+        introScript: require('../assets/font/intro-script-demo-medium.otf')
+    });
+
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const onSubmit = () => {
+        if ((regex.test(email) === false) && email) {
+            setError("Invalid email address")
+        }  else if (username.length < 4) {
+            setError("The username must have a minimum length of 4 characters")
+        } else if (password.length < 6) {
+            setError("The password must have a minimum length of 6 characters")
+        } else {
+            setError("")
+            console.log("C'est bon")
+        }
+    }
+
+    if (!fontsLoaded)
+        return null
+
     return (
         <SafeAreaView style={{justifyContent: "center", alignItems: "center", marginTop: 30}}>
+            {error && (
+                <Text
+                    style={{
+                        color: "red",
+                        fontWeight: "bold",
+                        fontSize: 13,
+                        textAlign: "center"
+                    }}
+                >
+                    {error}
+                </Text>
+
+            )}
             <View>
                 <Text style={styles.label}>Username</Text>
                 <TextInput
-                    placeholder={"Enter username..."}
+                    placeholder={"Enter an username..."}
                     style={styles.input}
+                    onChangeText={setUsername}
+                    autoCorrect={false}
                 />
 
                 <Text style={styles.label}>E-mail</Text>
                 <TextInput
-                    placeholder={"Enter email..."}
+                    placeholder={"Enter an email..."}
                     style={styles.input}
+                    autoCorrect={false}
+                    onChangeText={setEmail}
                 />
 
                 <Text style={styles.label}>Password</Text>
                 <TextInput
-                    placeholder={"Enter password..."}
+                    placeholder={"Enter a password..."}
                     secureTextEntry={true}
                     style={styles.input}
+                    onChangeText={setPassword}
                 />
             </View>
             <TouchableOpacity
@@ -34,28 +84,40 @@ export default function Register({navigation}) {
                     justifyContent: "center",
                     alignItems: "center",
                 }}
-                onPress={() => navigation.navigate("Login")}
+                onPress={() => onSubmit()}
             >
-                <Text style={{color: "white", fontWeight: "bold", fontSize: 15}}>Sign Up</Text>
+                <Text
+                    style={{
+                        color: "white",
+                        fontSize: 15,
+                        fontWeight: "bold",
+                    }}
+                >
+                    Sign Up
+                </Text>
             </TouchableOpacity>
-            
-            <Text
-                style={{
-                    color: "blue",
-                    textAlign: "center",
-                    textDecorationLine: "underline"
-                }}
-            >
-                Already got an account ? Login here.
-            </Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+                <Text
+                    style={{
+                        color: "blue",
+                        textAlign: "center",
+                        textDecorationLine: "underline",
+                        fontFamily: "oxygen_bold",
+                        marginTop: 20
+                    }}
+                >
+                    Already got an account ? Login here
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     label: {
-        fontWeight: "bold",
-        marginTop: 10
+        marginTop: 10,
+        fontFamily: "oxygen_bold",
     },
     input: {
         backgroundColor: "white",
