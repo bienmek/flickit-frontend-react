06a585c3bdec8ef156f7e1a222cc9cd500502d62
TitouXTv <thibault.velly@gmail.com>
2023-01-24 13:16:20 +0100

    changement profil

--- App.js
+++ App.js
@@ -7,9 +7,7 @@ import Profile from "./screens/Profile";
 import Ranking from "./screens/Ranking";
 import CameraScreen from "./screens/CameraScreen"
 import ObjectContextProvider from "./context/objectContext";
-<<<<<<< HEAD
 import Settings from "./screens/Settings";
-=======
 import ConfirmationScreen from "./screens/ConfirmationScreen";
 import ImageViewer from "./screens/ImageViewer";
 import Loading from "./components/Loading";
@@ -18,28 +16,10 @@ import {onAuthStateChanged} from "firebase/auth";
 import {auth} from "./firebase";
 import {UserContextProvider} from "./context/userContext";
 import EmailVerification from "./screens/EmailVerification";
->>>>>>> 72a078a5ae308529e0fd64cc9102a7c56aabe58e
 
 const Stack = createStackNavigator()
 
 export default function App() {
-<<<<<<< HEAD
-  return (
-      <NavigationContainer>
-          <ObjectContextProvider>
-              <Stack.Navigator initialRouteName={"Register"}>
-                  <Stack.Screen name={"Sign Up"} component={Register} options={{animationEnabled: false, headerShown: false}}/>
-                  <Stack.Screen name={"Log In"} component={Login} options={{headerShown: false}}/>
-                  <Stack.Screen name={"Home"} component={Home} options={{animationEnabled: false, headerShown: false}}/>
-                  <Stack.Screen name={"Profile"} component={Profile} options={{animationEnabled: false, headerShown: false}}/>
-                  <Stack.Screen name={"Ranking"} component={Ranking} options={{animationEnabled: false, headerShown: false}}/>
-                  <Stack.Screen name={"CameraScreen"} component={CameraScreen} options={{animationEnabled: false, headerShown: false}}/>
-                  <Stack.Screen name={"Settings"} component={Settings} options={{headerShown: false}}/>
-              </Stack.Navigator>
-          </ObjectContextProvider>
-      </NavigationContainer>
-  );
-=======
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -74,6 +54,7 @@ export default function App() {
                             <Stack.Screen name={"CameraScreen"} component={CameraScreen} options={{animationEnabled: false, headerShown: false}}/>
                             <Stack.Screen name={"ConfirmationScreen"} component={ConfirmationScreen} options={{headerShown: false}}/>
                             <Stack.Screen name={"ImageViewer"} component={ImageViewer} options={{headerShown: false}}/>
+                            <Stack.Screen name={"Settings"} component={Settings} options={{headerShown: false}}/>
                         </Stack.Navigator>
                     </UserContextProvider>
                 </ObjectContextProvider>
@@ -93,5 +74,4 @@ export default function App() {
         </NavigationContainer>
   )
 
->>>>>>> 72a078a5ae308529e0fd64cc9102a7c56aabe58e
 }
